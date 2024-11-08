7ad530283f54c4ce009c139806d9c31cdd244392
jiadsii <45071541+jiadsii@users.noreply.github.com>
2024-11-08 16:20:47 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -900,7 +900,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://rust.ziek.vip:21114".to_owned()
+    "http://8.138.101.3:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
